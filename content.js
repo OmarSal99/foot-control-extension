c80652bd75c35c53a57a25190c0b167faec59ba0
NabeelKhu <Nabeel.khuffash@exalt.ps>
2024-01-17 15:09:53 +0200

    update content and fix async support in service-w

--- content.js
+++ content.js
@@ -8,108 +8,7 @@ const ACTIONS = {
   INPUT_KEY_PRESSED: "input key pressed",
 };
 
-// HID devices supported
-const DEVICES_LIST = [
-  Object.freeze({
-    device: "hpMouse",
-    vendorId: 0x03f0,
-    usagePage: 0x0001,
-  }),
-  Object.freeze({
-    device: "joystick",
-    vendorId: 0x0079,
-    productId: 0x0006,
-    usagePage: 0x0001,
-  }),
-];
-
-// if it is true the input from the input keys will be sent to the popup otherwise it will be sent to service-worker
-let forwardInputToPopup = false;
-// there is no way to know if the popup is closed, the timer here will consider it closed in the last msg from it was 1000ms ago
-let popupTimer;
-
-//check if the char is a number, used to test demo app against number keys in keyboard
-// TODO: remove it in production
-function isSingleDigitNumber(str) {
-  return /^[0-9]$/.test(str);
-}
-
-// inform other parts that a key is pressed,  if isPPopupOpen is true the message should be sent to it
-function handleKeyInput(key) {
-  if (chrome.runtime?.id) {
-    console.log("from inside!!");
-    if (forwardInputToPopup) {
-      chrome.runtime.sendMessage({
-        action: ACTIONS.INPUT_KEY_PRESSED,
-        key: key,
-      });
-    } else {
-      chrome.runtime.sendMessage({
-        action: ACTIONS.KEY_EVENT,
-        key: key,
-      });
-    }
-  }
-}
-// adding keylisteners to run when a key is pressed
-document.addEventListener("keydown", (event) => {
-  if (isSingleDigitNumber(event.key)) handleKeyInput(event.key);
-});
-
-function startPopupTimer() {
-  // Clear the existing timer (if any)
-  clearTimeout(popupTimer);
-
-  // Set a new timer for 3 seconds
-  popupTimer = setTimeout(function () {
-    // Your function to be called after 3 seconds of inactivity
-    forwardInputToPopup = false;
-    console.log("popupclosed!");
-  }, 1000);
-}
-
-// recieve messages from other parts of the code
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log(message);
-  if (message.action == ACTIONS.REFRESH_PAGE) {
-    location.reload();
-  } else if (message.action == ACTIONS.TAB) {
-    const currentFocusedElement = document.activeElement;
-    const allInputFields = document.querySelectorAll('input[type="text"]');
-    const currentIndex = Array.from(allInputFields).indexOf(
-      currentFocusedElement
-    );
-
-    // Move focus to the next input field
-    const nextIndex = (currentIndex + 1) % allInputFields.length;
-    allInputFields[nextIndex].focus();
-  } // request button in the pop up is pressed
-  else if (message.action == ACTIONS.REQUEST_DEVICE) {
-    const devicesSupported = DEVICES_LIST.map((device) => {
-      return { vendorId: device.vendorId, usagePage: device.usagePage };
-    });
-    navigator.hid
-      .requestDevice({ filters: devicesSupported })
-      .then(async (devices) => {
-        const handledDevice = devices[0];
-        await handledDevice.open();
-        console.log(handledDevice);
-        handledDevice.addEventListener("inputreport", (event) => {
-          gamepadControllerEntryHandler.onHIDEntry(event);
-        });
-        console.log("Connected to HID device:", devices);
-      })
-      .catch((error) => {
-        console.error("Error connecting to HID device:", error);
-      });
-
-    // request device logic and start capture
-    handleKeyInput();
-  } else if (message.action == ACTIONS.POPUP_IN_INPUT_FIELD) {
-    forwardInputToPopup = true;
-    startPopupTimer();
-  }
-});
+const LAST_CONNECTED_DEVICE_LOCAL_STORAGE_KEY = "last connnected HID device";
 
 /**
  * Wraps the controller event handling related logic.
@@ -146,3 +45,166 @@ const gamepadControllerEntryHandler = (function () {
     onHIDEntry,
   };
 })();
+
+// HID devices supported
+const DEVICES_LIST = [
+  Object.freeze({
+    device: "hpMouse",
+    vendorId: 0x03f0,
+    productId: 0x0150,
+    usagePage: 0x0001,
+  }),
+  Object.freeze({
+    device: "joystick",
+    vendorId: 0x0079,
+    productId: 0x0006,
+    usagePage: 0x0001,
+    entryHandler: gamepadControllerEntryHandler.onHIDEntry,
+  }),
+];
+
+// if it is true the input from the input keys will be sent to the popup otherwise it will be sent to service-worker
+let forwardInputToPopup = false;
+// there is no way to know if the popup is closed, the timer here will consider it closed in the last msg from it was 1000ms ago
+let popupTimer;
+
+//check if the char is a number, used to test demo app against number keys in keyboard
+// TODO: remove it in production
+function isSingleDigitNumber(str) {
+  return /^[0-9]$/.test(str);
+}
+
+// inform other parts that a key is pressed,  if isPPopupOpen is true the message should be sent to it
+function handleKeyInput(key) {
+  if (chrome.runtime?.id) {
+    console.log("from inside!!");
+    if (forwardInputToPopup) {
+      chrome.runtime.sendMessage({
+        action: ACTIONS.INPUT_KEY_PRESSED,
+        key: key,
+      });
+    } else {
+      chrome.runtime.sendMessage({
+        action: ACTIONS.KEY_EVENT,
+        key: key,
+      });
+    }
+  }
+}
+
+/**
+ * Opens the device and binds its inputreport event to its handler, in case no device
+ *     was passed, then the device stored in local storage will be loaded.
+ *
+ * @param {?HIDDevice} device The HID to be opened and binded to its entry handler
+ */
+const connectDevice = async (device) => {
+  let deviceToBind = device;
+  // In case no device was passed then get the device stored in local storage
+  if (!device) {
+    const savedDevice = JSON.parse(
+      localStorage.getItem(LAST_CONNECTED_DEVICE_LOCAL_STORAGE_KEY)
+    );
+
+    if (!savedDevice) {
+      return;
+    }
+    const devicesWithPermissions = await navigator.hid.getDevices();
+    deviceToBind = devicesWithPermissions.filter((deviceElement) => {
+      return (
+        deviceElement.productId == savedDevice.productId &&
+        deviceElement.vendorId == savedDevice.vendorId
+      );
+    })[0];
+  } else {
+    deviceToSave = { vendorId: device.vendorId, productId: device.productId };
+    localStorage.setItem(
+      LAST_CONNECTED_DEVICE_LOCAL_STORAGE_KEY,
+      JSON.stringify(deviceToSave)
+    );
+  }
+  try {
+    await deviceToBind.open();
+    bindDeviceEntry(deviceToBind);
+  } catch (error) {
+    console.error("Failed to open HID device");
+  }
+};
+
+/**
+ * Binds the entry handler of the device using the devices list to the HID
+ *     opened
+ *
+ * @param {HIDDevice} device The HID opened for entry listening
+ */
+const bindDeviceEntry = (hidDevice) => {
+  const deviceUnderUse = DEVICES_LIST.filter((deviceElement) => {
+    return (
+      deviceElement.productId == hidDevice.productId &&
+      deviceElement.vendorId == hidDevice.vendorId
+    );
+  })[0];
+  hidDevice.addEventListener("inputreport", (event) => {
+    deviceUnderUse.entryHandler(event);
+  });
+};
+
+// adding keylisteners to run when a key is pressed
+document.addEventListener("keydown", (event) => {
+  if (isSingleDigitNumber(event.key)) handleKeyInput(event.key);
+});
+
+// Will try to see if there's some device saved in local storage in order to
+// open it and bind its inputreport event to its handler
+connectDevice();
+
+function startPopupTimer() {
+  // Clear the existing timer (if any)
+  clearTimeout(popupTimer);
+
+  // Set a new timer for 3 seconds
+  popupTimer = setTimeout(function () {
+    // Your function to be called after 3 seconds of inactivity
+    forwardInputToPopup = false;
+    console.log("popupclosed!");
+  }, 1000);
+}
+
+// recieve messages from other parts of the code
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(message);
+  if (message.action == ACTIONS.REFRESH_PAGE) {
+    location.reload();
+  } else if (message.action == ACTIONS.TAB) {
+    const currentFocusedElement = document.activeElement;
+    const allInputFields = document.querySelectorAll('input[type="text"]');
+    const currentIndex = Array.from(allInputFields).indexOf(
+      currentFocusedElement
+    );
+
+    // Move focus to the next input field
+    const nextIndex = (currentIndex + 1) % allInputFields.length;
+    allInputFields[nextIndex].focus();
+  } // request button in the pop up is pressed
+  else if (message.action == ACTIONS.REQUEST_DEVICE) {
+    const devicesSupported = DEVICES_LIST.map((device) => {
+      return {
+        vendorId: device.vendorId,
+        productId: device.productId,
+        usagePage: device.usagePage,
+      };
+    });
+    navigator.hid
+      .requestDevice({ filters: devicesSupported })
+      .then(async (devices) => {
+        const handledDevice = devices[0];
+        connectDevice(handledDevice);
+      })
+      .catch((error) => {
+        console.error("Error connecting to HID device:", error);
+      });
+  } else if (message.action == ACTIONS.POPUP_IN_INPUT_FIELD) {
+    forwardInputToPopup = true;
+    startPopupTimer();
+  }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,42 @@
 import { ACTIONS } from "./actions.js";
 let keyMapping = {};
+let debuggerQueue = [];
+
+// this should never be the case but if there is any very rare case apear code can handle it
+// a max waiting time before the key input is dropped
+const MAX_WAITING_TIME = 3000;
+// max loops before key input is dropped
+const MAX_LOOPS = 1000;
 
 const KEY_MAPPING_SERVICE_WORKER_LOCAL_STORAGE =
   "footPedalKeyMappingBackGround";
-
+let sendingOutput = null;
+let isLocked = false;
+let idCounter = 0;
 // send command and return a promise, the promise is resolved when sending command is done to do clean up
 function sendCommand(tabs, key) {
   return new Promise((resolve, reject) => {
-    chrome.debugger.sendCommand(
-      { tabId: tabs[0].id },
-      "Input.dispatchKeyEvent",
-      {
-        type: "keyDown",
-        windowsVirtualKeyCode: key,
-        nativeVirtualKeyCode: key,
-        macCharCode: key,
-        text: String.fromCharCode(key),
-      },
-      () => {
-        // test it it actually wait
-        //setTimeout(()=>{resolve();}, 2000);
+    chrome.debugger.attach({ tabId: tabs[0].id }, "1.0", async function () {
+      await new Promise((resolve, reject) => {
+        chrome.debugger.sendCommand(
+          { tabId: tabs[0].id },
+          "Input.dispatchKeyEvent",
+          {
+            type: "keyDown",
+            windowsVirtualKeyCode: key,
+            nativeVirtualKeyCode: key,
+            macCharCode: key,
+            text: String.fromCharCode(key),
+          },
+          () => {
+            resolve();
+          }
+        );
+      });
+      chrome.debugger.detach({ tabId: tabs[0].id }, () => {
         resolve();
-      }
-    );
+      });
+    });
   });
 }
 
@@ -51,46 +65,73 @@ chrome.runtime.onMessage.addListener(async function (
   if (message.action == ACTIONS.KEY_EVENT) {
     let outputKeys = keyMapping[message.key];
     if (!(Array.isArray(outputKeys) && outputKeys.length > 0)) return;
-    chrome.tabs.query({ active: true }, function (tabs) {
-      chrome.debugger.attach({ tabId: tabs[0].id }, "1.0",async function () {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      async function (tabs) {
         let i = 0;
+        let lastPromise = null;
         for (i = 0; i < outputKeys.length; i++) {
           let key = outputKeys[i].key;
           let keycode = parseInt(outputKeys[i].keycode, 10);
-          if (key.length === 1) {
-            await sendCommand(tabs, keycode);
-          } else {
-            switch (key) {
-              case "F5":
-                console.log("refresh page !");
-                chrome.tabs.query(
-                  { active: true, currentWindow: true },
-                  function (tabs) {
+          let myId = idCounter++;
+          let process = async (key, keycode, myId) => {
+            let promise = null;
+            debuggerQueue.push(myId);
+            let timeStarted = new Date().getTime();
+            let numberOfLoops = 0;
+            while (isLocked || debuggerQueue[0] != myId) {
+              if (
+                numberOfLoops > MAX_LOOPS ||
+                timeStarted + MAX_WAITING_TIME < new Date().getTime()
+              ) {
+                console.log("enter special case !!!");
+                try {
+                  debuggerQueue.splice(debuggerQueue.indexOf(myId), 1);
+                } catch {}
+                return;
+              }
+              await sendingOutput;
+              numberOfLoops++;
+            }
+            isLocked = true;
+            debuggerQueue.shift();
+            if (key.length === 1) {
+              promise = sendCommand(tabs, keycode);
+            } else {
+              switch (key) {
+                case "F5":
+                  console.log("refresh page !");
+                  promise = new Promise((resolve, reject) => {
                     chrome.tabs.sendMessage(tabs[0].id, {
                       action: ACTIONS.REFRESH_PAGE,
                     });
-                  }
-                );
-                break;
-              case "Tab":
-                console.log("tab pressed !");
-                chrome.tabs.query(
-                  { active: true, currentWindow: true },
-                  function (tabs) {
+                    resolve();
+                  });
+                  break;
+                case "Tab":
+                  console.log("tab pressed !");
+                  promise = new Promise((resolve, reject) => {
                     chrome.tabs.sendMessage(tabs[0].id, {
                       action: ACTIONS.TAB,
                     });
-                  }
-                );
-                break;
-              default:
-                break;
+                    resolve();
+                  });
+                  break;
+                default:
+                  break;
+              }
             }
-          }
+            if (i + 1 == outputKeys.length) lastPromise = promise;
+            sendingOutput = promise;
+            await promise;
+            isLocked = false;
+          };
+          process(key, keycode, myId);
         }
-        chrome.debugger.detach({ tabId: tabs[0].id });
-      });
-    });
+        //ignore the error, it's a promise but ide don't think so
+        await lastPromise;
+      }
+    );
   }
 });
 
